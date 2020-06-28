0b4951aa6427d0c8d72a1e8ca8e75ce5a25d3436
Jacob Lowe <jacoblowe2.0@gmail.com>
2020-06-28 13:50:21 -0700

    add in more test and remove old test backup

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 90,
+      statements: 90,
+    },
+  },
   verbose: true,
   testMatch: ['**/src/?(*.)+(spec|test).js'],
   moduleNameMapper: {
